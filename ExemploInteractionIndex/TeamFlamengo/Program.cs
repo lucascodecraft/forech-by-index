8b2ffd6f4491032a1c087f07e357c12e105d6bf7
lucaslima10077 <lucaslima10077@gmail.com>
2020-02-27 11:32:09 -0300

    Add project files.

---
+++ ExemploInteractionIndex/TeamFlamengo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemploInteractionIndex
+{
+    class Program
+    {
+        static void Main()
+        {
+            var teamFlamengo = new string[] { "Arrascaeta", "Bruno Henrique", "Everton Ribeiro", "Gabriel Barbosa", "Gerson" };
+
+            foreach (var player in teamFlamengo[0..3])
+            {
+                Console.WriteLine($"{player}");
+            }
+        }
+    }
+}
